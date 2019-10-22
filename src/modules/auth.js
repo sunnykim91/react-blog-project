c8eaa55997d13ec3dfee29c16a6f0b0e579d46f1
unknown <tjsgnl2002@gmail.com>
2019-10-22 16:21:51 +0900

    login, register UI develop

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,14 +1,42 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+    CHANGE_FIELD,
+    ({ form, key, value }) => ({
+        form, // reigister, login
+        key, // username, password, passwordConfirm
+        value, // 실제 바꾸려는 값
+    })
+);
 
-const initialState = {};
+export const initializeForm = createAction(INITALIZE_FORM, form => form);  // register, login
+
+const initialState = {
+    register: {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+    },
+    login: {
+        username: '',
+        password: '',
+    },
+};
 
 const auth = handleActions(
     {
-        [SAMPLE_ACTION]: (state, action) => state,
+        [CHANGE_FIELD]: (state, { payload: {form, key, value} }) => 
+        produce(state, draft => {
+            draft[form][key] = value;   // 예: state.register.username을 바꾼다.
+        }),
+        [INITALIZE_FORM]: (state, { payload: form }) => ({
+            ...state, 
+            [form]: initialState[form],
+        }),
     },
     initialState,
 );
